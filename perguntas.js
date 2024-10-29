6230d8f0f531f72daa7b5a8fb6ec41d7c8deac56
emillyrodriguesciscoto <132910044+emillyrodriguesciscoto@users.noreply.github.com>
2024-10-29 20:26:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Deuses gregos',
+    'Quem era a figura conhecida como Medusa?',
+    'uma gorgona'
+)
+
+criaCartao(
+    'Deuses gregos',
+    'Os Misterios de Eleusis eram um festival religioso dedicado a quais divindades gregas?',
+    ' Persefone e Demeter'
+)
+
+criaCartao(
+    'Deuses gregos',
+    'Um mito grego narra a paixao de Apolo pela ninfa Dafne, mas esse amor de Apolo nao foi correspondido. Para fugir de Apolo, Dafne assumiu qual forma?',
+    ' De um loureiro'
+)
+
+criaCartao(
+    'Deuses gregos',
+    'Duas importantes fontes de conhecimento sobre a mitologia grega sao os poemas epicos Iliada e Odisseia. Quem e considerado o autor desses poemas??',
+    'Homero'
+)
